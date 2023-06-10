ecb15737fbff247a94ef2abc9fc641d6bf49789c
Big Boss <bigboss@metalrodeo.xyz>
2023-06-09 19:53:03 -0500

    slower rollout, faster destroy

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,10 +2,13 @@
 
 linodes=$(linode-cli linodes ls | grep bencheth | awk '{print $2}' | grep -vE "^$|id")
 
+linode_names=$(linode-cli linodes ls | grep bencheth | awk '{print $4}' | grep -vE "^$|label")
+
+echo "🔥 Destroying linode ${linode_names}"
+echo "Press Ctrl+C to cancel or enter to continue"
+read
+
 for linode in $linodes; do
-  linode_name=$(linode-cli linodes view $linode | awk '{print $4}' | grep -vE "^$|label")
-  echo "🔥 Destroying linode $linode_name"
-  echo "Press Ctrl+C to cancel or enter to continue"
-  read
   linode-cli linodes delete $linode
+  sleep 2
 done
